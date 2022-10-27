c8f5b92593cb9cbe36517ac4b8aa42950a3a9d31
Abdullah Al-Mofleh <almofleh.abdullah@gmail.com>
2022-10-27 11:31:26 +0400

    Fixing a bug in footer icon

--- resources/views/components/frontend/layouts/footer.blade.php
+++ resources/views/components/frontend/layouts/footer.blade.php
@@ -25,27 +25,49 @@
         <div class="col-12 col-sm-12 col-md-12 col-lg-5 d-flex justify-content-end align-items-center">
           <div class="footer-social">
             <ul class="list--inline site-footer__social-icons social-icons">
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Facebook"><i class="icon icon-facebook"></i></a>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Facebook">
+                  <i class="icon icon-facebook"></i>
+                  <span class="icon__fallback-text">Facebook</span>
+
+                </a>
               </li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Twitter"><i class="icon icon-twitter"></i>
-                  <span class="icon__fallback-text">Twitter</span></a></li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Pinterest"><i class="icon icon-pinterest"></i>
-                  <span class="icon__fallback-text">Pinterest</span></a></li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Instagram"><i class="icon icon-instagram"></i>
-                  <span class="icon__fallback-text">Instagram</span></a></li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Tumblr"><i class="icon icon-tumblr-alt"></i>
-                  <span class="icon__fallback-text">Tumblr</span></a></li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on YouTube"><i class="icon icon-youtube"></i>
-                  <span class="icon__fallback-text">YouTube</span></a></li>
-              <li><a class="social-icons__link" href="#" target="_blank"
-                   title="Belle Multipurpose Bootstrap 4 Template on Vimeo"><i class="icon icon-vimeo-alt"></i>
-                  <span class="icon__fallback-text">Vimeo</span></a></li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Twitter">
+                  <i class="icon icon-twitter"></i>
+                  <span class="icon__fallback-text">Twitter</span>
+                </a>
+              </li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Pinterest">
+                  <i class="icon icon-pinterest"></i>
+                  <span class="icon__fallback-text">Pinterest</span>
+                </a>
+              </li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Instagram">
+                  <i class="icon icon-instagram"></i>
+                  <span class="icon__fallback-text">Instagram</span>
+                </a>
+              </li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Tumblr">
+                  <i class="icon icon-tumblr-alt"></i>
+                  <span class="icon__fallback-text">Tumblr</span>
+                </a>
+              </li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title=" YouTube">
+                  <i class="icon icon-youtube"></i>
+                  <span class="icon__fallback-text">YouTube</span>
+                </a>
+              </li>
+              <li>
+                <a class="social-icons__link" href="#" target="_blank" title="Vimeo">
+                  <i class="icon icon-vimeo-alt"></i>
+                  <span class="icon__fallback-text">Vimeo</span>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
