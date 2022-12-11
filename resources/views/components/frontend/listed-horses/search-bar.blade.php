e0646e3d509caed938e12871253c448612e5ed35
Abdullah Almofleh <almofleh.abdullah@gmail.com>
2022-12-11 22:24:10 +0400

    Finish search filter

---
+++ resources/views/components/frontend/listed-horses/search-bar.blade.php
@@ -0,0 +1,28 @@
+<div class="custom-search">
+  <form onsubmit="handelQuerySubmit(event)" class="input-group search-header search" role="search"
+        style="position: relative;">
+    <input class="search-header__input search__input input-group__field" value="{{ request()->query('query') }}"
+           type="search" id="search-listed-horses" name="query" placeholder="Search" aria-label="Search"
+           autocomplete="off">
+    <span class="input-group__btn">
+      <button class="btnSearch" type="submit">
+        <i class="icon fa-solid fa-magnifying-glass"></i>
+      </button>
+    </span>
+  </form>
+</div>
+
+
+@push('scripts')
+  <script>
+    function handelQuerySubmit(event) {
+      event.preventDefault();
+      const searchEl = document.querySelector('#search-listed-horses');
+      const query = searchEl.value;
+
+      var url = new URL('{!! route('listed_horses.list', request()->query()) !!}');
+      url.searchParams.set('query', query);
+      window.location = url.href;
+    }
+  </script>
+@endpush
