36800cd73ec49eb9de41e8af9f0fabea626a6906
Abdullah Almofleh <almofleh.abdullah@gmail.com>
2022-12-25 03:17:18 +0400

    Fix date problem

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
   'sex'              => ['male' => 1, 'female' => 0],
   'blogs'            => ['status'=>['published' => 1, 'draft' => 0]],
-  'horse_birth_year' => ['start' => 2010, 'end' => intval(date('Y'))],
+  'horse_birth_year' => ['start' => 2000, 'end' => intval(date('Y'))],
   'order_status'     => [
     'pending'  => 'pending',
     'finished' => 'finished',
